879f5733b2867b015ba62fbbed48114bf74c3cd3
Ryan W <wirer3@gmail.com>
2021-02-18 15:07:50 -0500

    This is the second commit where all values are input by scanner

--- bank/src/BankAccountTester.java
+++ bank/src/BankAccountTester.java
@@ -3,12 +3,12 @@ public class BankAccountTester {
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		double amount = 0;
 		System.out.println("Enter the starting balance of your bank account: ");
-		amount = in.nextDouble();
-		BankAccount myBankAccount = new BankAccount(amount);
-		myBankAccount.withdraw(500.00);
-		myBankAccount.deposit(1500.00);
+		BankAccount myBankAccount = new BankAccount(in.nextDouble());
+		System.out.println("Enter the amount you would like to withdraw: ");
+		myBankAccount.withdraw(in.nextDouble());
+		System.out.println("Enter the amount you would like to deposit: ");
+		myBankAccount.deposit(in.nextDouble());
 		System.out.println("The balance of your account is " + myBankAccount.getBalance());
 	}
 
