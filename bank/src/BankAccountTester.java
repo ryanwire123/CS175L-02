ebb2c35b076c4c134a31796d12259cda1fef6469
Ryan W <wirer3@gmail.com>
2021-03-01 19:48:17 -0500

    Modified Bank account with interest

--- bank/src/BankAccountTester.java
+++ bank/src/BankAccountTester.java
@@ -4,12 +4,18 @@ public class BankAccountTester {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter the starting balance of your bank account: ");
-		BankAccount myBankAccount = new BankAccount(in.nextDouble());
-		System.out.println("Enter the amount you would like to withdraw: ");
-		myBankAccount.withdraw(in.nextDouble());
+		System.out.println("Enter the interest rate for this account: ");
+		BankAccount myBankAccount = new BankAccount(in.nextDouble(), in.nextDouble());
 		System.out.println("Enter the amount you would like to deposit: ");
 		myBankAccount.deposit(in.nextDouble());
-		System.out.println("The balance of your account is " + myBankAccount.getBalance());
+		System.out.println("Enter the amount you would like to withdraw: ");
+		myBankAccount.withdraw(in.nextDouble());
+		System.out.println("Enter the amount you would like to withdraw: ");
+		myBankAccount.withdraw(in.nextDouble());
+		System.out.println("If you want to generate interest, enter 'Yes'");
+		myBankAccount.getInterest(in.next());
+		
+		
 	}
 
 	
